7da2023d2558659b7314c1264a5f6a8a8a896702
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-03-12 19:48:56 -0600

    main

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from app import parse
 from app import views
 
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from flask import render_template, request, redirect
+from app.parse import PDF
 from app import app
 import os
 
-app.config['UPLOAD_FOLDER'] = "/home/andy/Documents/WebApp/app/tmp"
+app.config['UPLOAD_FOLDER'] = "app/tmp"
+app.config['ALLOWED_FILE_EXTENSIONS'] = ["pdf"]
 
 @app.route("/")
 def index():
@@ -22,14 +24,23 @@ def submit():
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
+    def allowed_files(filename):
+        if not filename or '.' not in filename:
+            return False
+        ext = filename.rsplit('.', 1)[1]
+        return ext.lower() in app.config['ALLOWED_FILE_EXTENSIONS'] 
+
     if request.method == "POST":
 
         if request.files:
 
             resume = request.files["pdf"]
-            path = os.path.join(app.config['UPLOAD_FOLDER'], "file")
+            if not allowed_files(resume.filename):
+                return render_template("submit.html")
+            filename = resume.filename.replace(' ', '_')
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             resume.save(path)
-
+            pdf = PDF(filename)
             print("Pdf saved!")
             return redirect(request.url)
 
