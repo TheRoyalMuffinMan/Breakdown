0d28252c4f902d1638f494b6ca35c08fb216459e
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-08-13 13:36:31 -0500

    work plz

--- service/routes.py
+++ service/routes.py
@@ -2,6 +2,6 @@
 from flask import send_from_directory
 from service import app
 
-@app.route("/", defaults={'path':''})
-def serve(path):
+@app.route("/home")
+def serve():
     return send_from_directory(app.static_folder,'index.html')
