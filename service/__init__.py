b9d1703990263b3f0a3f68d77a10ec878e1bc7a8
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-08-13 13:40:12 -0500

    work plz

--- service/__init__.py
+++ service/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from flask_cors import CORS
 
 app = Flask(
     __name__, 
     static_folder="../client/build", 
     static_url_path=''
 )
-CORS(app)
 
 from service import routes
 from service import parse
--- service/routes.py
+++ service/routes.py
@@ -2,6 +2,6 @@
 from flask import send_from_directory
 from service import app
 
-@app.route("/home")
+@app.route("/")
 def serve():
-    return send_from_directory(app.static_folder,'index.html')
+    return "hello word"
